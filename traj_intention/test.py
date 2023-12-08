393c406bca9d5030b334af0bdcfefc39e5c392ab
peiqiyu <peiqiyu1019@gmail.com>
2023-12-07 19:00:48 -0500

    add repeat

--- traj_intention/test.py
+++ traj_intention/test.py
@@ -3,7 +3,7 @@ import os
 import torch
 from pathlib import Path
 FILE_DIR = Path(__file__).resolve().parent
-import numpy as np
+import numpy as np, scipy.stats as st
 import cv2
 from tqdm import tqdm
 import json
@@ -19,6 +19,14 @@ from Dataset import MyDataset, INTENTION_LIST
 from predict import IntentionPredictor
 
 os.environ["DISPLAY"]=":0"
+
+def compute_mean_and_conf_interval(accuracies, confidence=.95):
+    accuracies = np.array(accuracies)
+    n = len(accuracies)
+    m, se = np.mean(accuracies), st.sem(accuracies)
+    h = se * st.t.ppf((1 + confidence) / 2., n-1)
+    return m, h
+
 if __name__ == '__main__':
     ROOT_DIR = f'{FILE_DIR}/../human_traj'
     
@@ -63,63 +71,72 @@ if __name__ == '__main__':
     else:
         JSON_FILE = f'{ROOT_DIR}/cut_traj_mask.json'
 
-    ckpt_path = f'{FILE_DIR}/checkpoints/seq{args.seq_len}_pred{args.pred_len}_epoch{args.epochs}_whole_{args.input_type}_{args.model_type}_nomask{args.no_mask}.pth'
-    dataset = MyDataset(JSON_FILE,ROOT_DIR,args,dataset_type=args.test_type)
-    dataloader = DataLoader(dataset, batch_size=1, shuffle=True)
-    print("data loaded!")
-    # pdb.set_trace()
-    count = 0
-    losses = 0
-    error = [0]*args.class_num
-    intention_list = []
-    labels_list = []
-    predictor = IntentionPredictor(model_type=args.model_type,no_mask=args.no_mask,filter_type=args.filter_type)
-    for batch in tqdm(dataloader):
-        inputs,target_traj,labels = batch # inputs.shape:[batch_size,seq_len,15*3]
-        # pred_traj,pred_intention = model(inputs)
-        pred_traj,pred_intention = predictor.predict(inputs,args.restrict)
-        gap = 1 if sum(abs(pred_intention-labels))>0 else 0
-        count += gap
-        if gap == 1:
-            error[labels[0]] += 1
-        # pdb.set_trace()
-        intention_list.append(pred_intention[0].item())
-        labels_list.append(labels[0].item())
-
-    intention_list = np.array(intention_list)
-    labels_list = np.array(labels_list)
-    confusion_matrix = metrics.confusion_matrix(labels_list,intention_list)
-    confusion_matrix_norm = metrics.confusion_matrix(labels_list,intention_list,normalize='true')
-    # pdb.set_trace()
-    # confusion_matrix_norm = confusion_matrix/confusion_matrix.sum(1)
-    cm_display_norm = metrics.ConfusionMatrixDisplay(confusion_matrix = confusion_matrix_norm, display_labels = ["no_action","connectors","screws","wheels"])
-    cm_display_norm.plot()
-    if args.save_fig:
-        if args.test_whole:
-            plt.savefig(f'{FILE_DIR}/results/cm_norm_test_whole_{args.restrict}_restrict_{args.test_type}_{args.model_type}_nomask{args.no_mask}.jpg', bbox_inches = 'tight')
-        else:
-            plt.savefig(f'{FILE_DIR}/results/cm_norm_not_test_whole_{args.restrict}_restrict_{args.test_type}_{args.model_type}_nomask{args.no_mask}.jpg', bbox_inches = 'tight')
-    plt.close()
-    cm_display = metrics.ConfusionMatrixDisplay(confusion_matrix = confusion_matrix, display_labels = ["no_action","connectors","screws","wheels"])
-    cm_display.plot()
-    # if args.save_fig:
-        # if args.test_whole:
-        #     plt.savefig(f'{FILE_DIR}/results/cm_test_whole_{args.restrict}_restrict_{args.test_type}_{args.model_type}_nomask{args.no_mask}.jpg', bbox_inches = 'tight')
-        # else:
-        #     plt.savefig(f'{FILE_DIR}/results/cm_not_test_whole_{args.restrict}_restrict_{args.test_type}_{args.model_type}_nomask{args.no_mask}.jpg', bbox_inches = 'tight')
-    count = count / len(dataset)
-    print(f"length of dataset:{len(dataset)}")
-    print(f"accuracy: {1 - count}")
-    print(f"loss:{losses/len(dataset)}")
-    print("dataset.weights")
-    print(dataset.weights)
-    print("error:")
-    error = [error[i]*dataset.weights[i] for i in range(len(dataset.weights))]
+    # ckpt_path = f'{FILE_DIR}/checkpoints/seq{args.seq_len}_pred{args.pred_len}_epoch{args.epochs}_whole_{args.input_type}_{args.model_type}_nomask{args.no_mask}.pth'
+    repeat_num = 10
+    acc_list = {}
     for key,value in INTENTION_LIST.items():
-        print(f"{key}: {1-error[value]}")
-    # print(error)
-            
+        acc_list[key] = []
+    acc_list["all"] = []
+    for i in range(repeat_num):
+        dataset = MyDataset(JSON_FILE,ROOT_DIR,args,dataset_type=args.test_type)
+        dataloader = DataLoader(dataset, batch_size=1, shuffle=True)
+        print("data loaded!")
+        # pdb.set_trace()
+        count = 0
+        losses = 0
+        error = [0]*args.class_num
+        intention_list = []
+        labels_list = []
+        predictor = IntentionPredictor(model_type=args.model_type,no_mask=args.no_mask,filter_type=args.filter_type)
+        for batch in tqdm(dataloader):
+            inputs,target_traj,labels = batch # inputs.shape:[batch_size,seq_len,15*3]
+            # pred_traj,pred_intention = model(inputs)
+            pred_traj,pred_intention = predictor.predict(inputs,args.restrict)
+            gap = 1 if sum(abs(pred_intention-labels))>0 else 0
+            count += gap
+            if gap == 1:
+                error[labels[0]] += 1
+            # pdb.set_trace()
+            intention_list.append(pred_intention[0].item())
+            labels_list.append(labels[0].item())
 
+        intention_list = np.array(intention_list)
+        labels_list = np.array(labels_list)
+        confusion_matrix = metrics.confusion_matrix(labels_list,intention_list)
+        confusion_matrix_norm = metrics.confusion_matrix(labels_list,intention_list,normalize='true')
+        # pdb.set_trace()
+        # confusion_matrix_norm = confusion_matrix/confusion_matrix.sum(1)
+        cm_display_norm = metrics.ConfusionMatrixDisplay(confusion_matrix = confusion_matrix_norm, display_labels = ["no_action","connectors","screws","wheels"])
+        cm_display_norm.plot()
+        if args.save_fig:
+            if args.test_whole:
+                plt.savefig(f'{FILE_DIR}/results/cm_norm_test_whole_{args.restrict}_restrict_{args.test_type}_{args.model_type}_nomask{args.no_mask}.jpg', bbox_inches = 'tight')
+            else:
+                plt.savefig(f'{FILE_DIR}/results/cm_norm_not_test_whole_{args.restrict}_restrict_{args.test_type}_{args.model_type}_nomask{args.no_mask}.jpg', bbox_inches = 'tight')
+        plt.close()
+        cm_display = metrics.ConfusionMatrixDisplay(confusion_matrix = confusion_matrix, display_labels = ["no_action","connectors","screws","wheels"])
+        cm_display.plot()
+        # if args.save_fig:
+            # if args.test_whole:
+            #     plt.savefig(f'{FILE_DIR}/results/cm_test_whole_{args.restrict}_restrict_{args.test_type}_{args.model_type}_nomask{args.no_mask}.jpg', bbox_inches = 'tight')
+            # else:
+            #     plt.savefig(f'{FILE_DIR}/results/cm_not_test_whole_{args.restrict}_restrict_{args.test_type}_{args.model_type}_nomask{args.no_mask}.jpg', bbox_inches = 'tight')
+        count = count / len(dataset)
+        print(f"length of dataset:{len(dataset)}")
+        print(f"accuracy: {1 - count}")
+        print(f"loss:{losses/len(dataset)}")
+        print("dataset.weights")
+        print(dataset.weights)
+        print("error:")
+        error = [error[i]*dataset.weights[i] for i in range(len(dataset.weights))]
+        for key,value in INTENTION_LIST.items():
+            print(f"{key}: {1-error[value]}")
+            acc_list[key].append(1-error[value].item())
+        acc_list["all"].append(1-count)
+
+    for key,value in acc_list.items():
+        acc_list[key] = compute_mean_and_conf_interval(value)
+    print(acc_list)
 
     
 
